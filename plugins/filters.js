a484a4ac29ee69913d08f3f8bb1f5746417f05ff
Devtokade <110349715+Devtokade@users.noreply.github.com>
2022-08-06 20:15:45 +0530

    Add files via upload

--- plugins/filters.js
+++ plugins/filters.js
@@ -1,94 +1,94 @@
-const { getFilter, bot, setFilter, deleteFilter, lydia } = require('../lib/')
-const fm = true
+/* Copyright (C) 2020 Yusuf Usta.
 
-bot(
-	{
-		pattern: 'stop ?(.*)',
-		fromMe: fm,
-		desc: 'Delete filters in chat',
-		type: 'group',
-		onlyGroup: true,
-	},
-	async (message, match) => {
-		if (!match) return await message.sendMessage(`*Example : .stop hi*`)
-		const isDel = await deleteFilter(message.jid, match)
-		if (!isDel)
-			return await message.sendMessage(`_${match} not found in filters_`)
-		return await message.sendMessage(`_${match} deleted._`)
-	}
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
+
+WhatsAsena - Yusuf Usta
+*/
+
+const Asena = require("../Utilis/events")
+const FilterDb = require("./sql/filters")
+// const config = require('../config');
+const Language = require("../language")
+const { prepareFilter } = require("../Utilis/greetings")
+const Lang = Language.getString("filters")
+let fm = true
+Asena.addCommand(
+  { pattern: "filter ?(.*)", fromMe: fm, desc: Lang.FILTER_DESC },
+  async (message, match) => {
+    match = message.message.match(/[\'\"](.*?)[\'\"]/gms)
+    if (match === null) {
+      filtreler = await FilterDb.getFilter(message.jid)
+      if (filtreler === false) {
+        await message.sendMessage(Lang.NO_FILTER)
+      } else {
+        var mesaj = Lang.FILTERS + "\n"
+        filtreler.map(
+          (filter) => (mesaj += "```=> " + filter.dataValues.pattern + "```\n")
+        )
+        await message.sendMessage(mesaj)
+      }
+    } else {
+      if (match.length < 2) {
+        return await message.sendMessage(
+          Lang.NEED_REPLY + " ```.filter 'sa' 'as'"
+        )
+      }
+      await FilterDb.setFilter(
+        message.jid,
+        match[0].replace(/['"]+/g, ""),
+        match[1].replace(/['"]+/g, ""),
+        match[0][0] === "'" ? true : false
+      )
+      await message.sendMessage(
+        Lang.FILTERED.format(match[0].replace(/['"]+/g, ""))
+      )
+    }
+  }
 )
 
-bot(
-	{
-		pattern: 'filter ?(.*)',
-		fromMe: fm,
-		desc: 'filter in groups',
-		type: 'group',
-		onlyGroup: true,
-	},
-	async (message, match) => {
-		match = match.match(/[\'\"](.*?)[\'\"]/gms)
-		if (!match) {
-			const filters = await getFilter(message.jid)
-			if (!filters)
-				return await message.sendMessage(
-					`_Not set any filter_\n*Example filter 'hi' 'hello'*`
-				)
-			let msg = ''
-			filters.map(({ pattern }) => {
-				msg += `=> ${pattern} \n`
-			})
-			return await message.sendMessage(msg.trim())
-		} else {
-			if (match.length < 2) {
-				return await message.sendMessage(`Example filter 'hi' 'hello'`)
-			}
-			await setFilter(
-				message.jid,
-				match[0].replace(/['"]+/g, ''),
-				match[1].replace(/['"]+/g, ''),
-				match[0][0] === "'" ? true : false
-			)
-			await message.sendMessage(
-				`_${match[0].replace(/['"]+/g, '')}_ added to filters.`
-			)
-		}
-	}
+Asena.addCommand(
+  { pattern: "stop ?(.*)", fromMe: fm, desc: Lang.STOP_DESC },
+  async (message, match) => {
+    match = message.message.match(/[\'\"](.*?)[\'\"]/gms)
+    if (match === null) {
+      return await message.sendMessage(
+        Lang.NEED_REPLY + "\n*Example:* ```.stop 'hello'```"
+      )
+    }
+
+    del = await FilterDb.deleteFilter(
+      message.jid,
+      match[0].replace(/['"]+/g, "")
+    )
+
+    if (!del) {
+      await message.sendMessage(Lang.ALREADY_NO_FILTER)
+    } else {
+      await message.sendMessage(Lang.DELETED)
+    }
+  }
 )
 
-bot(
-	{
-		on: 'text',
-		fromMe: false,
-		type: 'filterOrLydia',
-	},
-	async (message, match) => {
-		const filters = await getFilter(message.jid)
-		if (filters)
-			filters.map(async ({ pattern, regex, text }) => {
-				pattern = new RegExp(regex ? pattern : `\\b(${pattern})\\b`, 'gm')
-				if (pattern.test(message.text)) {
-					await message.sendMessage(text, {
-						quoted: message.data,
-					})
-				}
-			})
-
-		const isLydia = await lydia(message)
-		if (isLydia)
-			return await message.sendMessage(isLydia, { quoted: message.data })
-	}
-)
-
-bot(
-	{
-		on: 'text',
-		fromMe: true,
-		type: 'lydia',
-	},
-	async (message, match) => {
-		const isLydia = await lydia(message)
-		if (isLydia)
-			return await message.sendMessage(isLydia, { quoted: message.data })
-	}
-)
+Asena.addCommand({ on: "text", fromMe: false }, async (message, match) => {
+  let filtreler = await FilterDb.getFilter(message.jid)
+  if (!filtreler) return
+  filtreler.map(async (filter) => {
+    pattern = new RegExp(
+      filter.dataValues.regex
+        ? filter.dataValues.pattern
+        : "\\b(" + filter.dataValues.pattern + ")\\b",
+      "gm"
+    )
+    if (pattern.test(message.message)) {
+      let { msg, MessageType } = await prepareFilter(filter.dataValues.text)
+      await message.sendMessage(
+        msg,
+        {
+          quoted: message.data,
+        },
+        MessageType
+      )
+    }
+  })
+})
